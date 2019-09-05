7d4f48346cf5f0f1058b357b6c07abde88c51885
Blagojche Grujoski <b.grujoski@esportsconstruct.com>
2019-09-05 12:15:42 +0200

    animated number removed from stats

--- src/components/CompareData.js
+++ src/components/CompareData.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-import AnimateNumber from 'react-native-animate-number';
-
 import g from '../ui/Grid';
 import u from '../ui/Ui';
 import t from '../ui/Typo';
@@ -26,10 +24,10 @@ const CompareData = props => (
                 <Text style={ t.stat }>Kills</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsOne.kills} timing="linear" countBy={150} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsOne.kills.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsTwo.kills} timing="linear" countBy={150} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsTwo.kills.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
         
@@ -38,10 +36,10 @@ const CompareData = props => (
                 <Text style={ t.stat }>Matches Played</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsOne.matchesplayed} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsOne.matchesplayed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsTwo.matchesplayed} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsTwo.matchesplayed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
 
@@ -50,10 +48,10 @@ const CompareData = props => (
                 <Text style={ t.stat }>Minutes Played</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsOne.minutesplayed} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsOne.minutesplayed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsTwo.minutesplayed} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsTwo.minutesplayed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
 
@@ -62,10 +60,10 @@ const CompareData = props => (
                 <Text style={ t.stat }>Place Top 1</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsOne.placetop1} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsOne.placetop1.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsTwo.placetop1} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsTwo.placetop1.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
 
@@ -74,10 +72,10 @@ const CompareData = props => (
                 <Text style={ t.stat }>Place Top 3</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsOne.placetop3} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsOne.placetop3.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsTwo.placetop3} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsTwo.placetop3.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
 
@@ -86,10 +84,10 @@ const CompareData = props => (
                 <Text style={ t.stat }>Place Top 10</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsOne.placetop10} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsOne.placetop10.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsTwo.placetop10} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsTwo.placetop10.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
 
@@ -98,10 +96,10 @@ const CompareData = props => (
                 <Text style={ t.stat }>Place Top 25</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsOne.placetop25} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsOne.placetop25.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsTwo.placetop25} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsTwo.placetop25.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
 
@@ -110,10 +108,10 @@ const CompareData = props => (
                 <Text style={ t.stat }>Players Outlived</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsOne.playersoutlived} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsOne.playersoutlived.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsTwo.playersoutlived} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsTwo.playersoutlived.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
 
@@ -122,10 +120,10 @@ const CompareData = props => (
                 <Text style={ t.stat }>Total Score</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsOne.score} timing="linear" countBy={1500} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsOne.score.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.statsTwo.score} timing="linear" countBy={1500} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.statsTwo.score.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
 
--- src/components/StatsData.js
+++ src/components/StatsData.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-import AnimateNumber from 'react-native-animate-number';
-
 import g from '../ui/Grid';
 import t from '../ui/Typo';
 import u from '../ui/Ui';
@@ -23,7 +21,7 @@ const StatsData = props => (
                 <Text style={ t.stat }>Kills</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.stats.kills} timing="linear" countBy={150} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.stats.kills.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
         
@@ -32,7 +30,7 @@ const StatsData = props => (
                 <Text style={ t.stat }>Matches Played</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.stats.matchesplayed} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.stats.matchesplayed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
 
@@ -41,7 +39,7 @@ const StatsData = props => (
                 <Text style={ t.stat }>Minutes Played</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.stats.minutesplayed} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.stats.minutesplayed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
 
@@ -50,7 +48,7 @@ const StatsData = props => (
                 <Text style={ t.stat }>Place Top 1</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.stats.placetop1} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.stats.placetop1.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
 
@@ -59,7 +57,7 @@ const StatsData = props => (
                 <Text style={ t.stat }>Place Top 3</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.stats.placetop3} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.stats.placetop3.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
 
@@ -68,7 +66,7 @@ const StatsData = props => (
                 <Text style={ t.stat }>Place Top 10</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.stats.placetop10} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.stats.placetop10.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
 
@@ -77,7 +75,7 @@ const StatsData = props => (
                 <Text style={ t.stat }>Place Top 25</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.stats.placetop25} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.stats.placetop25.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
 
@@ -86,7 +84,7 @@ const StatsData = props => (
                 <Text style={ t.stat }>Players Outlived</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.stats.playersoutlived} timing="linear" countBy={100} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.stats.playersoutlived.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
 
@@ -95,7 +93,7 @@ const StatsData = props => (
                 <Text style={ t.stat }>Total Score</Text>
             </View>
             <View style={[ g.f, g.abs, u.tRow, u.tableRow ]}>
-                <AnimateNumber style={ t.opt } value={props.stats.score} timing="linear" countBy={1500} interval={1} formatter={(val) => { return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }} />
+                <Text style={ t.opt }>{ props.stats.score.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</Text>
             </View>
         </View>
 
